92a245395c4e73345c0207d307a45f975006b0ac
rimahatib51-star <rimahatib51@gmail.com>
2026-06-12 20:42:56 +0300

    Add showMessage function to display project info

---
+++ script.js
@@ -0,0 +1,4 @@
+function showMessage() {
+    document.getElementById("message").innerHTML =
+        "Bu proje Python ve Neo4j ile çalışan console uygulamasıdır. Çalıştırmak için movie_graph.py dosyasını indirip Neo4j açıkken Python ile çalıştırmalısınız.";
+}
